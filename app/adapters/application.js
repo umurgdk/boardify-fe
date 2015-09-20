7d9a2ff80ff39efa9889333f76d9a32df6ba28a3
Umur Gedik <umurgdk@gmail.com>
2015-09-20 23:49:10 +0200

    add csrf tokens to request

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-  namespace: 'api/v1'
+  namespace: 'api/v1',
+  headers: Ember.computed(function () {
+    return {
+      "X-XSRF-TOKEN": decodeURIComponent(Ember.get(document.cookie.match(/XSRF\-TOKEN\=([^;]*)/), "1"))
+    }
+  })
 });
