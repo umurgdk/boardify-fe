346ba1d57a8173efbcf1e01c104102c801167714
Umur Gedik <umurgdk@gmail.com>
2015-09-20 23:48:31 +0200

    voting

--- app/components/card-item.js
+++ app/components/card-item.js
@@ -4,5 +4,14 @@ export default Ember.Component.extend({
   style: Ember.computed('model', function () {
     const columnColor = this.get('columnColor');
     return new Ember.Handlebars.SafeString(`background: ${columnColor}`)
-  })
+  }),
+
+  actions: {
+    voteUp() {
+      const card = this.model;
+
+      card.incrementProperty('votes');
+      card.save();
+    }
+  }
 });
